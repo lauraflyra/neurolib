041da398f5b3e5e55130312550dc63a38a6cd9a6
Laura <lauraflyra@gmail.com>
2022-06-06 13:01:56 +0200

    partial eeg Model

--- neurolib/models/eeg/model.py
+++ neurolib/models/eeg/model.py
@@ -1,22 +1,82 @@
+import numpy as np
 
-# comment to test my git skills
+import mne
+from mne.datasets import eegbci
+from mne.datasets import fetch_fsaverage
+import os.path as op
+
+###################
+
+# In a different file or functions or whatever, the user would be able to set up their bem and source space, and then
+# those are already given to the class
+
+
+
 
 class EEGModel:
 
-    def __init__(self, params, N, subject="fsaverage", trans=None, src=None, bem=None, info = None):
+    def __init__(self, params, N, subject="fsaverage", subject_dir = None, trans=None, src=None, bem=None, raw_fname = None):
 
         self.subject = subject
-        #subject should always be a string
+        # subject should always be a string
+        # assert subject is string
+
+        self.subject_dir = subject_dir
+        # have supported data file and path
+
         self.src = src
-        self.trans= trans #do we calculate trans if not given?? Or do we use fsaverage trans?
+        # assert src is correct file type
+        self.trans = trans  # do we calculate trans if not given?? Or do we use fsaverage trans?
         self.bem = bem
-        self.info = info #info gives electrodes positions
+        # assert bem is correct file type
+
+        if self.subject == "fsaverage":
+            self.loadSubjectData()
+
+        self.raw_fname = raw_fname #info gives electrodes positions
+        #check that raw_fname is in the correct format
+        # assert raw_fname is edf file
+
+        if self.raw_fname is None:
+            # we use what's in example in mne
+            self.raw_fname, = eegbci.load_data(subject=1, runs=[6])
+        self.loadRawData()
         pass
 
 
     def loadSubjectData(self):
-        open data file
-        #have supported data file
+
+        fs_dir = fetch_fsverage(verbose=True)
+        subjects_dir = op.dirname(fs_dir)
+        self.trans = "fsaverage"
+        if self.src is None:
+            self.src = op.join(fs_dir, 'bem', 'fsaverage-ico-5-src.fif')
+        if self.bem is None:
+            self.bem = op.join(fs_dir, 'bem', 'fsaverage-5120-5120-5120-bem-sol.fif')
+
+
+
+    def loadRawData(self):
+        raw = mne.io.read_raw_edf(self.raw_fname, preload=True)
+
+        # Clean channel names to be able to use a standard 1005 montage
+        new_names = dict(
+            (ch_name,
+             ch_name.rstrip('.').upper().replace('Z', 'z').replace('FP', 'Fp'))
+            for ch_name in raw.ch_names)
+        raw.rename_channels(new_names)
+
+        # Read and set the EEG electrode locations, which are already in fsaverage's
+        # space (MNI space) for standard_1020:
+        montage = mne.channels.make_standard_montage('standard_1005')
+        raw.set_montage(montage)
+        raw.set_eeg_reference(projection=True)  # needed for inverse modeling
+
+    def set_src(self):
+        pass
+
+    def set_bem(self):
+        pass
 
     def run(self,  activity, append):
         #this is supposed to do the matrix multiplication leadfield @ activity
@@ -85,5 +145,7 @@ class Model:
         bold = False,
         eeg = False,
         ....):
+
+        #here model.EEGModel should already be initialized somewhere!!
 
         pass
