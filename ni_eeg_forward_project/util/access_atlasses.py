939964049fdfade9e5953312cd9d20e8d77e945f
Martin <martinkrueck@gmx.de>
2022-06-30 18:25:39 +0200

    - was not numba compatible, but first check of runtime seems that without jit-compilation the runtime is already fine

--- ni_eeg_forward_project/util/access_atlasses.py
+++ ni_eeg_forward_project/util/access_atlasses.py
@@ -1,16 +1,24 @@
 import nibabel
-import numba
 import numpy as np
 
 
 # TODO: the function has to perform well enough to do checks for 1000-10000 dipoles.
-def get_labels_of_points(points, atlas="aal2"):
+def get_labels_of_points(points: np.ndarray, atlas="aal2") -> tuple[bool, np.ndarray, list[str]]:
     """ Gives labels of regions the points fall into.
         :points : ndarray of points defined in MNI space (mm).
         :return : (was a label found, label-code, label-string)
         :rtype: (bool, int, str)
     """
-    # load
+    n_points = points.shape[0]
+    label_codes = np.zeros(n_points)    # Remark: or expand points-array by one dimension and fill label-codes in there?
+
+    points_expanded = np.ones((n_points, 4))    # expand by a column with ones only to allow for transformations
+    points_expanded[:, 0:3] = points            # with affines
+
+    if not points.shape[1] == 3:
+        raise ValueError
+
+    # load atlas (integer encoded volume and string-labels)
     if atlas == "aal2":
         atlas_img = nibabel.load("./datasets/aal/aal2.nii.gz")
         atlas_labels = None
@@ -21,19 +29,20 @@ def get_labels_of_points(points, atlas="aal2"):
     affine_inverse = np.linalg.inv(affine)  # mni to "voxel" space
 
     # get voxel codes
-    atlas_img.get_fdata()
+    codes = atlas_img.get_fdata()
+    for point_idx, point in enumerate(points_expanded):
+        back_proj = get_backprojection(point, affine, affine_inverse)
+        label_codes[point_idx] = codes[int(back_proj[0]), int(back_proj[1]), int(back_proj[2])]
 
-    return
+    return label_codes
 
 
-@numba.njit()
-def get_backprojection(point, affine, affine_inverse):
+def get_backprojection(point_expanded: np.ndarray, affine, affine_inverse):
     """ Transform MNI-mm-point into voxel-number.
         :point : 3d point in MNI-coordinate space (mm)
     """
     # Remark: This function assumes continuous (no gaps) definition of the atlas.
 
-    point_expanded = np.hstack((point, 1))  # expand to apply matrix multiplications with the affine
     back_proj = affine_inverse @ point_expanded    # project the point from mni to voxel
 
     # round to voxel resolution
