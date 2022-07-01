1c1867f2647b1c79053f5234671ed2f5b7ff0539
Martin <martinkrueck@gmx.de>
2022-07-01 18:31:49 +0200

    - test for label assignment to point with AAL2 atlas
    - label assignment to points with AAL2 atlas ( passed test)

--- ni_eeg_forward_project/util/access_atlases.py
+++ ni_eeg_forward_project/util/access_atlases.py
@@ -1,38 +1,63 @@
 import nibabel
 import numpy as np
 import logging
-from xml.dom import minidom
+from xml.etree import ElementTree
+
+
+def create_label_lut(path: str) -> dict:
+    """ Create a lookup table that contains "anatomical acronyms" corresponding to the encodings of the regions
+        specified by the used anatomical atlas. Adds an empty label for code "0" if not specified otherwise by atlas.
+
+        :param : ToDo: proper variable naming depending of the final implementation.
+        :return : Dictionary with keys being the integer codes of regions and the values being
+    """
+
+    # Look up the codes ("index") and the names of the regions defined by the atlas.
+    tree = ElementTree.parse(path)
+    root = tree.getroot()
+    label_lut = {}
+    for region in root.find("data").findall("label"):
+        label_lut[region.find("index").text] = region.find("name").text
+
+    if "0 "not in label_lut.keys():
+        label_lut["0"] = ""
+    return label_lut
 
 
 # TODO: the function has to perform well enough to do checks for 1000-10000 dipoles.
 def get_labels_of_points(points: np.ndarray, atlas="aal2") -> tuple[list[bool], np.ndarray, list[str]]:
     """ Gives labels of regions the points fall into.
-        :points : ndarray of points defined in MNI space (mm).
-        :return : (was a label found, label-code, label-string)
-        :rtype: (bool, int, str)
+
+        :param points : ndarray of points defined in MNI space (mm).
+        :param atlas :  Specification of the anatomical atlas. Remark: Currently only AAL2 is supported.
+        :return :       Tuple. First element is a list of bools, indicating for each point if a valid assignment within
+                        the space defined by the atlas was found. Second element array of the assigned label-codes.
+                        Third, list of strings of the "anatomical acronyms".
+
     """
     n_points = points.shape[0]
     label_codes = np.zeros(n_points)    # Remark: or expand points-array by one dimension and fill label-codes in there?
     label_strings = [None] * n_points
     points_found = [None] * n_points
 
-    points_expanded = np.ones((n_points, 4))    # expand by a column with ones only to allow for transformations
-    points_expanded[:, 0:3] = points            # with affines
+    points_expanded = np.ones((n_points, 4))    # Expand by a column with ones only to allow for transformations
+    points_expanded[:, 0:3] = points            # with affines.
 
     if not points.shape[1] == 3:
         raise ValueError
 
-    # load atlas (integer encoded volume and string-labels)
+    # ToDo: make sure relative paths work.
+    # Load atlas (integer encoded volume and string-labels).
     if atlas == "aal2":
-        atlas_img = nibabel.load("./datasets/aal/atlas/AAL2.nii")
-        atlas_labels = None
+        atlas_img = nibabel.load("AAL2.nii")
+        atlas_labels_lut = create_label_lut("AAL2.xml")
     else:
-        raise
+        raise ValueError
 
-    affine = atlas_img.affine               # transformation from voxel to mni-space
-    affine_inverse = np.linalg.inv(affine)  # mni to "voxel" space
+    affine = atlas_img.affine               # Transformation from voxel- to mni-space.
+    affine_inverse = np.linalg.inv(affine)  # Transformation mni- to "voxel"-space.
 
-    # get voxel codes
+    # Get voxel codes
     codes = atlas_img.get_fdata()
     for point_idx, point in enumerate(points_expanded):
         back_proj = get_backprojection(point, affine, affine_inverse)
@@ -41,28 +66,35 @@ def get_labels_of_points(points: np.ndarray, atlas="aal2") -> tuple[list[bool], 
             label_codes[point_idx] = codes[int(back_proj[0]), int(back_proj[1]), int(back_proj[2])]
 
         except IndexError:
-            logging.error("")
+            logging.error("The atlas does not specify an assignment for the given MNI-coordinate.")
             label_codes[point_idx] = np.NAN
 
         if np.isnan(label_codes[point_idx]):
             points_found[point_idx] = False
-            label_strings[point_idx] = ""
+            label_strings[point_idx] = "invalid"
         else:
             points_found[point_idx] = True
-            label_strings[point_idx] = "label"
+            label_strings[point_idx] = atlas_labels_lut[str(int(label_codes[point_idx]))]   # ToDo: clean up type-
+                                                                                            # conversions.
 
     return points_found, label_codes, label_strings
 
 
-def get_backprojection(point_expanded: np.ndarray, affine, affine_inverse):
+def get_backprojection(point_expanded: np.ndarray, affine, affine_inverse) -> np.ndarray:
     """ Transform MNI-mm-point into voxel-number.
-        :point : 3d point in MNI-coordinate space (mm)
+
+        :param point_expanded : 4x1 array. First elements being the 3d point in MNI-coordinate space (mm),
+                                last element being a 1 for the offset in transformations.
+        :param affine:          4x4 matrix. Projects voxel-numbers to MNI coordinate space (mm).
+        :param affine_inverse:  4x4 matrix. Back projection from MNI space.
+        :return :               4x1 array. The point projected back into "voxel-number-space", last element 1.
     """
     # Remark: This function assumes continuous (no gaps) definition of the atlas.
 
     back_proj = affine_inverse @ point_expanded    # project the point from mni to voxel
 
-    # round to voxel resolution
+    # Round to voxel resolution, multiplication with elements inverse is equivalent to division with elements of the
+    # affine here.
     back_proj_rounded = np.round(np.diag(affine_inverse) * back_proj, 0) * np.diag(affine)
 
     return back_proj_rounded
--- tests/test_access_atlases.py
+++ tests/test_access_atlases.py
@@ -9,9 +9,15 @@ class TestAccessAtlases(unittest.TestCase):
 
         # An arbitrary testing point is the MNI-coordinate (-6, 14, 0). E.g. mentioned in doi:10.1093/brain/awt329
         # that the left caudate nucleus is located there.
-        test_point = np.array([[-6, 14, 0]])    # one sample point with known label
-        labels = access_atlases.get_labels_of_points(test_point, atlas="aal2")
-
+        test_point = np.array([[-6, 14, 0]])    # One sample point with known label.
+        result = access_atlases.get_labels_of_points(test_point, atlas="aal2")
         expected_result = ([True], np.array([7001, ]), ["Caudate_L"])
-        self.assertTupleEqual(expected_result, labels)
+        self.assertTupleEqual(expected_result, result)
 
+        # A point that should be way out of the space covered by any brain-atlas.
+        expected_result = ([False], np.array([np.NAN, ]), ["invalid"])
+        test_point = np.array([[1000, 0, 0]])
+        points_valid, codes, acronyms = access_atlases.get_labels_of_points(test_point, atlas="aal2")
+        self.assertEqual(expected_result[0], points_valid)
+        assert np.isnan(codes[0])
+        self.assertEqual(expected_result[2], acronyms)
