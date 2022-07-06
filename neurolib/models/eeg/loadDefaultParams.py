26f8f2cbe28184e19a33458dc1703ac85d8c52fd
janak <janakoehler@outlook.com>
2022-07-07 00:47:29 +0200

    debug params_eeg in model.py aand loadDefaultParams (was always overwritten with None before), add example usecase for poster in test_EEGModelClass.ipynb, add new TODOs

--- neurolib/models/eeg/loadDefaultParams.py
+++ neurolib/models/eeg/loadDefaultParams.py
@@ -5,28 +5,26 @@ from neurolib.utils.collections import dotdict
 # Since the user should only be able to change the conductances, the type of the sources and the pos/spacing
 # we only need to have in the loadDefaultParams.py those four values
 
-def loadDefaultParams(conductances=None, type_scr=None, scr_pos=None, scr_spacing=None, sfreq = None):
+def loadDefaultParams(params_eeg):
     """Load default parameters for the EEG Model
 
     :return: A dictionary with the default parameters of the model
     :rtype: dict
     """
-
-    params_eeg = dotdict({})
-
-    if conductances is None:
+    count = 0
+    if params_eeg.conductances is None:
         params_eeg.eeg_conductances = (0.3, 0.006, 0.3)
-    if type_scr is None:
+    if params_eeg.type_scr is None:
         params_eeg.eeg_type_scr = "volumetric"
-    if scr_pos is None:  # source positions refers to volumetric sources
+    if params_eeg.scr_pos is None:  # source positions refers to volumetric
+        # sources
         params_eeg.eeg_scr_pos = 5.0
-    if scr_spacing is None:  # scr spacing refers to surface sources
+    if params_eeg.scr_spacing is None:  # scr spacing refers to surface sources
         params_eeg.eeg_scr_spacing = "oct6"
-    if sfreq is None: #sfreq refers to the sample rate of the data using when creating the info file with the montage
+    if params_eeg.sfreq is None: #sfreq refers to the sample rate of the
+        # data using when creating the info file with the montage
         params_eeg.eeg_montage_sfreq = 256
 
-
-    return params_eeg
 
 # TODO: insert in the README the info about what scr_pos and scr_spacing and that the user should always write in the params dictionary following this nomenclature rule
 # TODO: insert in README that if scr_spacing is changed, but the scr_type is not, then we are still gonna use volumetric with standard value for it
--- neurolib/models/eeg/model.py
+++ neurolib/models/eeg/model.py
@@ -1,49 +1,48 @@
 import numpy as np
 import mne
-import os.path as op
 import loadDefaultParams as dp
 from neurolib.utils.collections import dotdict
 
 
 class EEGModel:
 
-    def __init__(self, params=None):
+    def __init__(self, params):
         """
         sfreq refers to the sample rate of the data using when creating the info file with the montage
         """
-        if params is None:
-            params = dotdict({})
+
+        params_eeg = dotdict({})
+        params_eeg.eeg_conductances = params.get("eeg_conductances")
+        params_eeg.eeg_type_scr = params.get("eeg_type_scr")
+        params_eeg.eeg_scr_pos = params.get("eeg_scr_pos")
+        params_eeg.eeg_scr_spacing = params.get("eeg_scr_spacing")
+        params_eeg.eeg_montage_sfreq = params.get("eeg_montage_sfreq")
 
         #TODO: check, if the person didn't change any params, we actually create anything new, we just load the fsaverage forward solution
+        # check if all values in params_eeg are None
+        dp.loadDefaultParams(params_eeg)
 
-        params_eeg = dp.loadDefaultParams(conductances = params.get(
-            "eeg_conductances"),
-                                      type_scr = params.get("eeg_type_scr"),
-                                      scr_pos=params.get("eeg_scr_pos"),
-                                      scr_spacing=params.get("eeg_scr_spacing"),
-                                      sfreq = params.get("eeg_montage_sfreq")
-                                      )
 
+        print(params_eeg, params_eeg.keys())
+
+        # TODO: get rid of this and acces trough params_eeg ! or get rid of
+        #  params_eeg
         self.conductances = params_eeg.eeg_conductances
         self.type_scr = params_eeg.eeg_type_scr
         self.scr_pos = params_eeg.eeg_scr_pos
         self.scr_spacing = params_eeg.eeg_scr_spacing
-        self.sfreq = params_eeg.eeg_montage_sfreq
+        self.sfreq = params_eeg.get('eeg_montage_sfreq')
 
-        # TODO: only give N and dt instead of params!
         self.N = params.get("N")  # this is number of nodes
         self.dt = params.get("dt")  # dt of input activity in ms
-        # TODO: check
         self.samplingRate_NDt = int(round(1/(self.sfreq * (self.dt/1000))))
 
         self.idxLastT = 0  # Index of the last computed t
-        # downsample (100 Hz
-        # EEG sampling rate)
+
         # TODO: somwhere compare N with the number of regions in the atlas. Assert they are the same
 
         # Since the user should only be able to change the conductances, the type of the sources and the pos/spacing
         # we only need to have in the loadDefaultParams.py those four values.
-
 
         # TODO: When the user doesnt change all params accordingly we should give a warning saying it's gonna run with default values
         # TODO: change directories the way that Nicola said
@@ -76,18 +75,20 @@ class EEGModel:
         return bem
 
     def set_src(self):
+        # TODO: catch for type being something wrong
         type = self.type_scr
         if type == 'surface':
             src = mne.setup_source_space(self.subject, subject_dir = self.subject_dir, spacing = self.scr_spacing,
                                     add_dist = "patch")
 
-        if type == 'volumetric':
+        elif type == 'volumetric':
             src = mne.setup_volume_source_space(subject=self.subject, bem=self.bem,
                                         subjects_dir=self.subject_dir,
                                         add_interpolator=False)
+
         return src
 
-    def downsampling(self, leadfield, atlas=None, averaging_method=None):
+    def downsampling_dummy(self, leadfield):
         # TODO: GO MARTIN
         #output size = self.N
         test = np.ones(leadfield.shape[0], self.N)
@@ -109,8 +110,7 @@ class EEGModel:
         print(leadfield.shape)
 
         # somewhere here should be the downsampling function
-        downsampled = self.downsampling(self, leadfield, atlas=None,
-                                        averaging_method=None)
+        downsampled = self.downsampling_dummy(self, leadfield)
         print(downsampled.shape)
         #which type of activity are we expecting here? Firing rates?
         # we need to think about units, it's in mV, conductivities also have units.
